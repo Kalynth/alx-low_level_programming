ee370fb8d3a9c553c9a04dffc3d0d7e7f8f67178
Kalynth <annmwangi.w@gmail.com>
2023-07-20 01:20:42 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,8 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 #include<stdio.h>
-void positive_or_ngegative(int i)
+void positive_or_ngegative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day, int year);
+
 #endif
