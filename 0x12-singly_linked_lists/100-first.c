18ee0b3064b60414d34bc49e95c01dcb21307775
Kalynth <annmwangi.w@gmail.com>
2023-08-26 02:16:36 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * main - Checks the code
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	return (0);
+}
