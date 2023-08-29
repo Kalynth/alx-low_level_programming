ad4814cc6df007bfee3b4d82fa957e376fe881fa
Kalynth <annmwangi.w@gmail.com>
2023-08-29 03:20:19 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,8 +43,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	if (idx == 0)
+	{
+		cake->next = *head;
 		*head = cake;
-
+		return (cake);
+	}
 	for (j = 0; j < idx - 1 && temp != NULL && idx != 0; j++)
 		temp = temp->next;
 	if (temp == NULL)
