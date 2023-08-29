c472732faac720901fe97fa3bb8f6729e12b2146
Kalynth <annmwangi.w@gmail.com>
2023-08-29 02:00:56 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+  * free_listint - Frees a list
+  * @head: Acts a pointer
+  * Return: Always 0 (Success)
+  */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temporary;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temporary);
+	}
+}
