da9c6f81246db0558cf736ee6c443a875f3a47e6
Kalynth <annmwangi.w@gmail.com>
2023-08-29 03:27:59 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,7 +14,7 @@ size_t print_listint(const listint_t *h)
 
 	for (tally = 0; h != NULL; tally++)
 	{
-		printf("The number of nodes:%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (tally);
