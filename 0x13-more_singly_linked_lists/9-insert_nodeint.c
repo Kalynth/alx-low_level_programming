2a5b5f225d3877cda6d7ce54941256bfd975949e
Kalynth <annmwangi.w@gmail.com>
2023-08-29 02:53:59 -0700

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+ /**
+  * Create_new_node - Creates a node
+  * @n: Contains data for the nod
+  * Return: The pointer to the node
+  */
+listint_t *create_new_node(int n)
+{
+	listint_t *new;
+
+	new  = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	return (new);
+}
+/**
+  * insert_nodeint_at_index - Inserts a node in an index
+  * @head: Acts as a pointer
+  * @idx: index
+  * @n: data of the node
+  * Return: The address of the node
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int j;
+	listint_t *temp;
+	listint_t *jod;
+	listint_t *cake;
+
+	temp = *head;
+	if (head == NULL)
+		return (NULL);
+	cake = create_new_node(n);
+	if (cake == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = cake;
+		return (cake);
+	}
+
+	if (idx == 0)
+		*head = cake;
+
+	for (j = 0; j < idx - 1 && temp != NULL && idx != 0; j++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
+	if (idx == 0)
+		cake->next = temp;
+	else
+	{
+		jod = temp->next;
+		temp->next = cake;
+		cake->next = jod;
+	}
+	return (cake);
+}
