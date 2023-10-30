00f61bba17911a84fd2dfd5dbe9133060e2d1fa4
Kalynth <annmwangi.w@gmail.com>
2023-10-30 11:41:18 +0000

    file I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * append_text_to_file - text to be appended at end of file
  * @filename: filename
@@ -25,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (letter_no = 0; text_content[letter_no]; letter_no++)
 			;
 
-		jod = write(fd, text_content, letter_no);
+		jod = write(kod, text_content, letter_no);
 
 		if (jod == -1)
 			return (-1);
